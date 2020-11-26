606c0bce88409514ee4f2690fbd1d04294eb58f7
Camila Atehortúa Sánchez <66139090+CamilaAtehortua@users.noreply.github.com>
2020-11-25 23:21:49 -0500

    cambio en la ruta

--- frontend/src/Routes/App.js
+++ frontend/src/Routes/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Pagina_inicial from "../Pages/pagina_inicial";
+import Iniciar_como from "../Pages/Iniciar_como";
+import Registrar_como from "../Pages/Registrar_como";
 import Registrarse from "../Pages/Registrarse";
 import Iniciar_sesion from "../Pages/Iniciar_sesion";
 import Iniciar_sesionA from "../Pages/Iniciar_sesionA";
 import RegistrarseA from "../Pages/RegistrarseA";
 import Evento from "../Pages/Evento";
-import Inf_evento from "../Pages/Inf_evento";
+import Inf_evento1 from "../Pages/Inf_evento1";
+import Inf_evento2 from "../Pages/Inf_evento2";
 import Seccion_adm from "../Pages/Seccion_adm";
 import Eventos_adm from "../Pages/Eventos_adm";
 import Subir_event from "../Pages/Subir_event";
@@ -17,13 +20,16 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/Pagina_inicial" component={Pagina_inicial} />
+          <Route exact path="/" component={Pagina_inicial} />
+          <Route exact path="/Iniciar_como" component={Iniciar_como} />
+          <Route exact path="/Registrar_como" component={Registrar_como} />
           <Route exact path="/Evento" component={Evento} />
           <Route exact path="/Registrarse" component={Registrarse} />
           <Route exact path="/Iniciar_sesion" component={Iniciar_sesion} />
           <Route exact path="/Iniciar_sesionA" component={Iniciar_sesionA} />
           <Route exact path="/RegistrarseA" component={RegistrarseA} />
-          <Route exact path="/Inf_evento" component={Inf_evento} />
+          <Route exact path="/Inf_evento1" component={Inf_evento1} />
+          <Route exact path="/Inf_evento2" component={Inf_evento2} />
           <Route exact path="/Seccion_adm" component={Seccion_adm} />
           <Route exact path="/Eventos_adm" component={Eventos_adm} />
           <Route exact path="/Subir_event" component={Subir_event} />
